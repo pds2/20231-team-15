58da94a6443c0321a6da78ae594f8c5428a87519
pedroaugtb <pedroaustoatb@gmail.com>
2023-07-03 00:51:02 -0300

    adicionei o main teste na minha branch

---
+++ spotify/src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "musica.cpp"
+#include "artista.cpp"
+
+//Main temporário para testar a classe Musica e Artista
+int main() {
+
+    Musica musicas;
+    
+    std::vector<Musica> listaMusicas = musicas.lerMusicasDoCSV("/home/pedroaugtb/codes/vscode/20231-team-15-1/musicas.csv");
+
+    // Função para imprimir os detalhes de todas as músicas
+    for (auto i = listaMusicas.begin(); i < listaMusicas.end(); i++) {
+        i->imprimirDetalhes();
+    }
+    
+    return 0;
+}

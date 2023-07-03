e7848208031f909200d873b6f8a6c53118c9d8a9
pedroaugtb <pedroaugustoatb@gmail.com>
2023-07-03 20:18:23 -0300

    adicionei default no artista.h

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,13 @@
 int main() {
 
     Musica musicas;
-    
+
     std::vector<Musica> listaMusicas = musicas.lerMusicasDoCSV("/home/pedroaugtb/codes/vscode/20231-team-15-1/musicas.csv");
 
     // Função para imprimir os detalhes de todas as músicas
     for (auto i = listaMusicas.begin(); i < listaMusicas.end(); i++) {
         i->imprimirDetalhes();
     }
-    
+
     return 0;
 }

b8f466319935436aa7cf613b73c586fce1c371c5
Victoria Estanislau <victoriaeramosdeoliveira@gmail.com>
2023-07-03 20:09:58 -0300

    mantendo apenas arquivos corretos

--- src/artista.cpp
+++
@@ -1,6 +0,0 @@
-#include "../include/artista.hpp"
-
-Album::Album(const int &id_artista, std::vector <int> lista_musicas) : ListaMusicas(lista_musicas) {
-    _id_artista = id_artista;
-    // ListaMusicas::m_lista_musicas = lista_musicas;
-};
--- src/usuario.cpp
+++
@@ -1,6 +0,0 @@
-#include "../include/usuario.hpp"
-
-Playlist::Playlist(const int &id_usuario, std::vector <int> lista_musicas) : ListaMusicas(lista_musicas) {
-    _id_usuario = id_usuario;
-    // ListaMusicas::m_lista_musicas = lista_musicas;
-};

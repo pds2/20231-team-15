cdbe2655d790163a9fb9197ed4416ebe574bfb7c
pedroaugtb <pedroaugustoatb@gmail.com>
2023-07-03 21:53:59 -0300

    nao sei

--- src/album.cpp
+++ src/album.cpp
@@ -6,10 +6,10 @@ std::string Album::get_autor(){
 
 Album::Album(Artista artista): _artista(artista) {}
 
-void Album::adicionar_musica(Musica m) override{
+void Album::adicionar_musica(Musica m) {
     for (Musica x : _lista_musica){
         if (m.getId() == x.getId()){
-            std::cout<< "Música já existente no álbum!" << std::endl
+            std::cout<< "Música já existente no álbum!" << std::endl;
         }
     }
     _lista_musica.push_back(m);

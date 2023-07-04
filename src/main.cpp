8cf61df6e72be6b733b4e011580e1959dfe4bf61
lucasdayrell <lucas-dayrell@hotmail.com>
2023-07-04 01:09:23 -0300

    eba

--- src/main.cpp
+++ src/main.cpp
@@ -15,9 +15,9 @@
 //Main temporário para testar a classe Musica e Artista
 int main() {
 
-    Musica musicas;
+    //Musica musicas;
 
-    std::vector<Musica> listaMusicas = musicas.ler_musicas_do_csv("/home/pedroaugtb/codes/vscode/20231-team-15-1/musicas.csv");
+    //std::vector<Musica> listaMusicas = musicas.ler_musicas_do_csv("/home/pedroaugtb/codes/vscode/20231-team-15-1/musicas.csv");
 
     // Função para imprimir os detalhes de todas as músicas
     //for (auto i = listaMusicas.begin(); i < listaMusicas.end(); i++) {
@@ -39,7 +39,24 @@ int main() {
     //if(a==0) std::cout << "Não há músicas repetidas" << std::endl;
 
     //Imprimir musica 101
-    listaMusicas[100].imprimir_detalhes();
+    //listaMusicas[100].imprimir_detalhes();    
+    std::string user, senha;
+    BancoUsuarios b = BancoUsuarios("../usuarios.txt");
+    b.adicionar_usuario("dayrinho123","lalala13"); 
+
+    std::cout << "Digite o Usuário:"<< std::endl;
+    std::cin>>user;
+    if (!b.verificar_usuario(user);){
+        std::cout<< "Usuário não existe amigão!!!"<< std::endl;
+    } else {
+        std::cout<<"Digite a Senha:"<< std::endl;
+        std::cin>> senha;
+        if (!b.verificar_senha(senha)){
+            std::cout<< "Senha errada"<< std::endl;
+        } else {
+           std::cout<< "Seja bem vindo"<< std::endl; 
+        }
+    }
 
     return 0;
 }

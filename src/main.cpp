2071c6ee66733b9ab3c03090c6e1e171165f3198
pedroaugtb <pedroaugustoatb@gmail.com>
2023-07-03 23:01:54 -0300

    mudei o csv

--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,24 @@ int main() {
     std::vector<Musica> listaMusicas = musicas.lerMusicasDoCSV("/home/pedroaugtb/codes/vscode/20231-team-15-1/musicas.csv");
 
     // Função para imprimir os detalhes de todas as músicas
+    //for (auto i = listaMusicas.begin(); i < listaMusicas.end(); i++) {
+    //    i->imprimir_detalhes();
+    //}
+
+    int a = 0;
+
+    // Função para ver se tem músicas repetidas
     for (auto i = listaMusicas.begin(); i < listaMusicas.end(); i++) {
-        i->imprimir_detalhes();
+        for (auto j = i + 1; j < listaMusicas.end(); j++) {
+            if (i->getTitulo() == j->getTitulo()) {
+                std::cout << "Música repetida: " << i->getTitulo() << std::endl;
+                a++;
+            }
+        }
     }
+
+    if(a==0) std::cout << "Não há músicas repetidas" << std::endl;
+
 
     return 0;
 }

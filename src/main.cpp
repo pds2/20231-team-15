dc153b94728f78d6d1f3cfec821738aa941e0570
lucasdayrell <lucas-dayrell@hotmail.com>
2023-07-04 15:33:42 -0300

    aa

--- src/main.cpp
+++ src/main.cpp
@@ -42,28 +42,29 @@ int main() {
     //listaMusicas[100].imprimir_detalhes();    
     std::string user, senha;
     BancoUsuarios b = BancoUsuarios("usuarios.txt");
-    b.adicionar_usuario("dayrinho123","lalala13"); 
+    b.adicionar_usuario("dayrinho123", "lalala13"); 
 
-    std::cout << "Digite o Usuário:"<< std::endl;
-    std::cin>>user;
-    std::cout << "Digite a Senha:" << std::endl;
-    std::cin>>senha;
-    while(1){
+    while (true) {
+        std::cout << "Digite o Usuário:" << std::endl;
+        std::cin >> user;
+        std::cout << "Digite a Senha:" << std::endl;
+        std::cin >> senha;
+
         try {
-        b.verificar_usuario(user);
-    } catch (usuario_nao_existe_e &u) {
-        std::cout<<"Usuário não existe. digite novamente"<<std::endl;
-    }
-    try {
-        b.verificar_senha(user,senha);
-    } catch (senha_incorreta_e &s){
-        std::cout<<"Senha Incorreta. digite novamente"<<std::endl;
-    }
-    if (b.verificar_senha(user,senha)){
-        std::cout<<"welcome!!"<<std::endl;
-        break;
-    }
-
+            if (!b.verificar_usuario(user)) {
+                throw usuario_nao_existe_e();
+            }
+            if (b.verificar_senha(user, senha)) {
+                std::cout << "Welcome!!" << std::endl;
+                break;
+            } else {
+                throw senha_incorreta_e();
+            }
+        } catch (usuario_nao_existe_e &u) {
+            std::cout << "Usuário não existe. Digite novamente." << std::endl;
+        } catch (senha_incorreta_e &s) {
+            std::cout << "Senha incorreta. Digite novamente." << std::endl;
+        }
     }
     Usuario u = Usuario(user, senha);
     return 0;

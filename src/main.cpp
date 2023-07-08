ce7d4cd447ff3866750927ecc792410bc14d4054
cinatit <cinatit.igor.santos@gmail.com>
2023-07-08 19:50:58 -0300

    Cria arquivo .xml que representa um music-item

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,6 @@ int main(int argc, char* argv[])
         i->imprimir_detalhes();
     }
 
-
     // Constroi app do GTK
     auto app = Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
 
@@ -38,6 +37,17 @@ int main(int argc, char* argv[])
     builder->get_widget("my_window", window);
     window->show_all();
     window->maximize();
+
+    // Widget lista de músicas
+    Gtk::ListBox* music_list_box = nullptr;
+    builder->get_widget("music-list-box", music_list_box);
+
+    // Widget musica-item
+    auto builder2 = Gtk::Builder::create();
+    builder2->add_from_file("music-item.xml");
+    Gtk::Widget* music_item = nullptr;
+    builder2->get_widget("music-item", music_item);
+    music_list_box->add(*music_item);
 
     return app->run(*window);
 }

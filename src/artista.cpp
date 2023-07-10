05c9d6e5c40fab2fe3dbe43517ca732f11ce21c6
pedroaugtb <pedroaugustoatb@gmail.com>
2023-07-10 01:28:53 -0300

    testes funcionando :)

--- src/artista.cpp
+++ src/artista.cpp
@@ -25,15 +25,15 @@ std::vector<Musica> Artista::get_artistas_musicas() const {
 }
 
 // Função para armazenar as músicas do artista
-void Artista::armazenar_musicas(const std::vector<Musica>& musicas) const{
-    std::vector<Musica> artistasMusicas;
+void Artista::armazenar_musicas(const std::vector<Musica>& musicas) {
     for (const auto& musica : musicas) {
-        if (Musica().get_artista() == _nome) {
-            artistasMusicas.push_back(musica);
+        if (musica.get_artista() == _nome) {
+            artistas_musicas.push_back(musica);
         }
     }
 }
 
+//
 void Artista::imprimir_artista() const {
     std::cout << "Id: " << _id_artista;
     std::cout << "  Nome: " << _nome << std::endl;
@@ -41,13 +41,12 @@ void Artista::imprimir_artista() const {
 
 void Artista::imprimir_discografia() const {
     std::cout << "Discografia de " << _nome << ":" << std::endl;
-    for (const auto& musica : Musica().ler_musicas_do_csv("/home/pedroaugtb/codes/vscode/20231-team-15/spotify/musicas.csv")) {
-        if (musica.get_artista() == _nome) {
-            std::cout << musica.get_titulo() << " - " << musica.get_album() << " (" 
+    for (const auto& musica : artistas_musicas) {
+        std::cout << musica.get_titulo() << " - " << musica.get_album() << " (" 
             << musica.get_ano() << ")" << std::endl;
-        }
     }
 }
+
 
 void Artista::imprimir_musicas(const std::vector<Musica>& musicas) const {
     std::cout << "Músicas de " << _nome << ":" << std::endl;

a7aeba5fe6e17e706e1d1e9d81c4c2827ea6c985
lucasdayrell <lucas-dayrell@hotmail.com>
2023-07-10 22:45:28 -0300

    não tem mais usuário

--- src/sistema.cpp
+++ src/sistema.cpp
@@ -190,46 +190,8 @@ void Sistema::exibir_discografia(std::vector <Artista> listar_artistas, std::vec
     }
 }
 
-
-// METODOS DA CLASSE USUARIO
-
-Usuario Sistema::login(BancoUsuarios b){
-        std::string username;
-        std::string senha;
-
-        while (true) {
-        std::cout << "Digite o Usuário:" << std::endl;
-        std::cin >> username;
-        std::cout << "Digite a Senha:" << std::endl;
-        std::cin >> senha;
-
-        try {
-            if (!b.verificar_usuario(username)) {
-                throw usuario_nao_existe_e();
-            }
-            if (b.verificar_senha(username, senha)) {
-                std::cout << "Welcome!!" << std::endl;
-                break;
-            } else {
-                throw senha_incorreta_e();
-            }
-        } catch (usuario_nao_existe_e &u) {
-            std::cout << "Usuário não existe. Digite novamente." << std::endl;
-        } catch (senha_incorreta_e &s) {
-            std::cout << "Senha incorreta. Digite novamente." << std::endl;
-        }
-    }
-    Usuario u = Usuario(username, senha);
-    return u;
-}
-
-//METODOS DA CLASSE BIBLIOTECA
-
-
-
 // METODOS DA CLASSE PLAYLIST
 
-//NÃO FUNCIONA BEM NO MAIN --- REVISAR (O ERRO NAO NECESSARIAMENTE TA AQUI)
 Playlist Sistema::criar_playlist(std::string user){
     std::string nome;
     std::cout << "Digite o nome da playlist:" << std::endl;
